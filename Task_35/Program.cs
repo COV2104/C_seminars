9e6a64fff49156c9144cee69448afbb33ea1ade7
COV <tsehlo@mail.ru>
2022-10-15 17:37:13 +0300

    Задача 35

---
+++ Task_35/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива,
+// значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] FillArray()
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < size; i++)
+    {
+        if (i < size - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.WriteLine($"{array[i]} ]\n");
+    }
+}
+
+int CountElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    
+    return count;
+}
+
+int[] massiv = FillArray();
+PrintArray(massiv);
+int counter = CountElements(massiv);
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10,99] равно {counter} \n");
