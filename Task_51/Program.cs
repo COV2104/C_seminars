e3261865a34645e5ba045e205e28dbe77573101e
COV <tsehlo@mail.ru>
2022-10-22 21:14:56 +0300

    Задача 51

---
+++ Task_51/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся 
+// на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Fill2DArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumDiagonal (int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+                result += array[i, j];
+        }
+    }
+    return result;
+}
+
+int[,] massiv = Fill2DArray(rows, columns);
+Console.WriteLine($"Двумерный массив {rows} x {columns} :");
+Console.WriteLine();
+Print2DArray(massiv);
+int sum = SumDiagonal(massiv);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, находящихся на главной диагонали = {sum}");
+Console.WriteLine();
+
