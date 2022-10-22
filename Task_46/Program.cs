f26c649dc2983322c21c9ee9d7667b60be9f2703
COV <tsehlo@mail.ru>
2022-10-22 21:13:30 +0300

    Задача 46

---
+++ Task_46/Program.cs
@@ -0,0 +1,39 @@
+﻿// Stanislav N: Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] Fill2DArray (int rows, int columns)
+{
+    int [,] array = new int [rows,columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(-100,101);
+        }
+    }
+    return array;
+}
+
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] massiv = Fill2DArray(rows,columns);
+Console.WriteLine($"Двумерный массив {rows} x {columns} :");
+Print2DArray(massiv);
