35019ec5c35117c5724b2bef88614c465b534b9c
COV <tsehlo@mail.ru>
2022-11-10 10:57:08 +0300

    Задача 55

---
+++ Task_55/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] Fill2DArray()
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    int[,] newArray = new int[array.GetLength(0), array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            newArray[i, j] = array[j, i];
+        }
+    }
+
+    return newArray;
+}
+
+if (rows == columns)
+{
+    Console.WriteLine($"Начальный массив {rows} x {columns}:");
+    int[,] massiv = Fill2DArray();
+    Print2DArray(massiv);
+    Console.WriteLine();
+    Console.WriteLine($"Измененный массив {rows} x {columns}:");
+    int[,] massiv1 = SortArray(massiv);
+    Print2DArray(massiv1);
+}
+else
+{
+    System.Console.WriteLine("Для данной сортировки массив должен быть квадратным");
+}
+
