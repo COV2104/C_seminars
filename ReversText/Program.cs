bf7959e93e24018fafdc8f6eafad3fde41e1980d
COV <tsehlo@mail.ru>
2022-11-12 16:48:08 +0300

    Разворот текста(рекурсия)

---
+++ ReversText/Program.cs
@@ -0,0 +1,16 @@
+﻿// Слово развернуть в обратном порядке
+System.Console.WriteLine("Введите текст: ");
+string text = Console.ReadLine();
+int i = text.Length - 1;
+
+void Revers(string text, int i)
+{
+    if (i >= 0)
+    {
+        Console.Write(text[i]);
+        Revers(text, i - 1);
+    }
+}
+Revers(text, i);
+System.Console.WriteLine();
+
